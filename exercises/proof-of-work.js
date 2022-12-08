9a247b9569d8db8d8d7a976ac482e325f943ece4
Kayode Oluwasegun <segunkayode00@gmail.com>
2022-12-08 09:06:19 +0100

    proof-of-work sample

---
+++ exercises/proof-of-work.js
@@ -0,0 +1,37 @@
+const SHA256 = require("crypto-js/sha256");
+const TARGET_DIFFICULTY =
+  BigInt(0x0fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff);
+const MAX_TRANSACTIONS = 10;
+
+const mempool = [];
+const blocks = [];
+
+function addTransaction(transaction) {
+  mempool.push(transaction);
+}
+
+function mine() {
+  const transactions = mempool.splice(-MAX_TRANSACTIONS);
+  const block = {
+    id: blocks.length,
+    transactions: transactions,
+    nonce: 0,
+  };
+  let hashedBlock = SHA256(JSON.stringify(block));
+  while (BigInt(`0x${hashedBlock}`) > TARGET_DIFFICULTY) {
+    block.nonce += 1;
+    hashedBlock = SHA256(JSON.stringify(block));
+  }
+
+  block.hash = hashedBlock;
+  blocks.push(block);
+}
+
+module.exports = {
+  TARGET_DIFFICULTY,
+  MAX_TRANSACTIONS,
+  addTransaction,
+  mine,
+  blocks,
+  mempool,
+};
