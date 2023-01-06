f87036d69689b2ca42fbf4675b76d6518fd70cf3
Kayode Oluwasegun <segunkayode00@gmail.com>
2023-01-06 21:12:14 +0100

    utxo example complete

--- eth/02_block_chain_storage/01_utxo.js
+++ eth/02_block_chain_storage/01_utxo.js
@@ -25,5 +25,19 @@ class Transaction {
       if (inputUTXO.spent === true)
         throw new Error("input includes a spent transaction");
     });
+
+    const totalInput = this.inputUTXOs.reduce((x, y) => x + y.amount, 0);
+    const totalOutput = this.outputUTXOs.reduce((x, y) => x + y.amount, 0);
+
+    console.log(totalInput);
+    console.log(totalOutput);
+
+    if (totalOutput > totalInput) throw new Error();
+
+    // transaction is successful at this point - mark input UTXOs as spent
+    this.inputUTXOs.forEach((inputUTXO) => inputUTXO.spend());
+
+    // use the remainder after output UTXOS as mining fee
+    this.fee = totalInput - totalOutput;
   }
 }
