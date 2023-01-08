e72206d794d52887182c319751fcacf3c109495d
Kayode Oluwasegun <segunkayode00@gmail.com>
2023-01-08 21:47:16 +0100

    recursively add items to BST

---
+++ eth/02_block_chain_storage/02_tree_dsa.js
@@ -0,0 +1,40 @@
+// Tree data structures are used heavily in blockchain development
+
+// Binary Search Tree - BST is a type of tree with the set of the following rules
+// - it's a binary tree ()
+// - the left side of the sub-node has nodes with keys that is lesser than the key of the parent node
+// - the right side of the sub-node has nodes with keys that is greater than the key of the parent node
+
+// BUILDING A BINARY SEARCH TREE
+class Node {
+  constructor(data) {
+    this.data = data;
+  }
+}
+
+class Tree {
+  constructor() {
+    // store reference to the tree's root
+    this.root = null;
+  }
+
+  addNode(node, parent = this.root) {
+    if (!this.root) {
+      this.root = node;
+      return;
+    }
+    if (node.data > parent.data) {
+      if (parent.right) {
+        this.addNode(node, parent.right);
+        return;
+      }
+      parent.right = node;
+    } else {
+      if (parent.left) {
+        this.addNode(node, parent.left);
+        return;
+      }
+      parent.left = node;
+    }
+  }
+}
